c7968db08f0af8da48c506778c3c8e3967e3e393
dleuiajs <jduejdb322@gmail.com>
2025-05-06 20:28:00 +0200

    zmena kontaktu v index.php

--- index.php
+++ index.php
@@ -347,22 +347,22 @@ loadPart("head");
          </div>
          <div class="row">
             <div class="col-md-10 offset-md-1">
-               <form id="request" class="main_form">
+               <form id="request" class="main_form" action="thxyou_contact.php" method="post">
                   <div class="row">
                      <div class="col-md-12 ">
-                        <input class="contactus" placeholder="Meno" type="type" name="Name">
+                        <input class="contactus" placeholder="Meno" type="text" name="meno" required>
                      </div>
                      <div class="col-md-12">
-                        <input class="contactus" placeholder="Email" type="type" name="Email">
+                        <input class="contactus" placeholder="Email" type="email" name="email" required>
                      </div>
                      <div class="col-md-12">
-                        <input class="contactus" placeholder="Telefónne číslo" type="type" name="Phone Number">
+                        <input class="contactus" placeholder="Telefónne číslo" type="tel" name="tel_cislo">
                      </div>
                      <div class="col-md-12">
-                        <textarea class="textarea" placeholder="Správa" type="type" Message="Name"></textarea>
+                        <textarea class="textarea" placeholder="Správa" type="text" , name="sprava" required></textarea>
                      </div>
                      <div class="col-md-12">
-                        <button class="send_btn">Odoslať</button>
+                        <button class="send_btn" type="submit">Odoslať</button>
                      </div>
                   </div>
                </form>
