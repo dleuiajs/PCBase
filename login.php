57c80b4ab416f92264c9b6ed76e3427e4ac6b552
dleuiajs <jduejdb322@gmail.com>
2025-05-21 21:26:26 +0200

    pri prihlasovaní a registrácii bola pridaná captcha

--- login.php
+++ login.php
@@ -12,23 +12,38 @@ loadPart("head");
     loadPart("header");
     ?>
     <!-- end header -->
-    <div class="container my-5" style="max-width: 400px;">
+    <?php
+    $rand1 = rand(1, 9);
+    $rand2 = rand(1, 9);
+    echo '<div class="container my-5" style="max-width: 400px;">
         <h2>Prihlásenie</h2>
         <form id="request" action="thxyou_login.php" method="post">
+            <input type="hidden" name="captcha1" value="' . $rand1 . '">
+            <input type="hidden" name="captcha2" value="' . $rand2 . '">
             <div class="form-group mb-3">
                 <label for="email">E-mail:</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Zadajte váš e-mail" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Zadajte váš e-mail"
+                    required>
             </div>
             <div class="form-group mb-4">
                 <label for="heslo">Heslo:</label>
-                <input type="password" class="form-control" id="heslo" name="heslo" placeholder="Zadajte vaše heslo" required>
+                <input type="password" class="form-control" id="heslo" name="heslo" placeholder="Zadajte vaše heslo"
+                    required>
+            </div>
+            <div class="form-group mb-4">
+                <label for="captcha">Koľko je ' . $rand1 . ' + ' . $rand2 . '?:</label>
+                <input type="number" min="0" class="form-control" id="captcha" name="captcha" placeholder="Overte, že nie ste robot"
+                    required>
             </div>
             <div class="d-flex align-items-center">
                 <button type="submit" class="btn btn-primary">Prihlásiť sa</button>
-                <a href="register.php" class="ms-3 text-decoration-underline" style="margin-left: 16px;">Registrovať sa</a>
+                <a href="register.php" class="ms-3 text-decoration-underline" style="margin-left: 16px;">Registrovať
+                    sa</a>
             </div>
-        </form>
-        <hr class="my-5">
+
+        </form>';
+    ?>
+    <hr class="my-5">
     </div>
     <!--  footer -->
     <?php
